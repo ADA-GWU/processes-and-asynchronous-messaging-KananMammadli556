d7352bb34e6e8f33e06e3925413ea05c6948924f
Kanan Mammadli <amega.lol@mail.ru>
2023-10-29 22:14:47 +0400

    Update Script.sql
    Fixed the code to run in a single run command

--- Script.sql
+++ Script.sql
@@ -1,9 +1,9 @@
-/*eate table ASYNC_MESSAGES(
+create table ASYNC_MESSAGES(
 RECORD_ID SERIAL,
 SENDER_NAME varchar(30),
 MESSAGE varchar(30),
 SENT_TIME TIMESTAMP,
-RECEIVED_TIME TIMESTAMP);*/
+RECEIVED_TIME TIMESTAMP),
 
-create user dist_user1;
-grant select, insert, update on ASYNC_MESSAGES to dist_user1
+create user dist_user1,
+grant select, insert, update on ASYNC_MESSAGES to dist_user1;
